ec09c8db0b988bcf36acbc74a29b869a2e1f9c10
Michael Tamirie <michaeltamirie@gmail.com>
2022-03-28 09:24:32 +0300

    Made some ammendment and add some components

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+class TodoContainer extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello Micky working here</h1>
+                <p>This is the first react component</p>
+            </div>
+        )
+    }
+}
+
+export default TodoContainer;
